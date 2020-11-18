fdd279f5f2e44005c7ff989ff804613f55e3080e
tosumaso <wakuwaku5712@gmail.com>
2020-11-18 19:24:19 +0900

    ログイン、ログアウト機能

--- db/migrate/20201117070747_create_posts.rb
+++ db/migrate/20201117070747_create_posts.rb
@@ -1,7 +1,9 @@
 class CreatePosts < ActiveRecord::Migration[6.0]
   def change
     create_table :posts do |t|
-
+      t.string :name, null: false
+      t.text :content, null: false
+      t.references :user, null: false, foreign_key: true
       t.timestamps
     end
   end
