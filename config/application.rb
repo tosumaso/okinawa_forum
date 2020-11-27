fee5a2fe4c51094838163883763dc483f2b6d09e
tosumaso <wakuwaku5712@gmail.com>
2020-11-27 16:13:46 +0900

    messageroom機能2

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,8 @@ module OkinawaForum
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
     config.action_view.automatically_disable_submit_tag= false
+    config.i18n.default_locale = :ja
+    config.time_zone = 'Tokyo'
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
